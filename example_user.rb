bb7a83b5674a0afe4c9d5f6b5865d0dc729ecc5b
Bryan <bryanbenson83@gmail.com>
2014-05-17 16:15:55 -0400

    Make a basic User model (including secure passwords

---
+++ example_user.rb
@@ -0,0 +1,13 @@
+class User
+	attr_accessor :name, :email
+
+	def initialize(attributes = {})
+		@name = attributes[:name]
+		@email = attributes[:name]
+	end
+
+	def formatted_email
+		"#{@name} <#{@email}>"
+	end
+		
+end
