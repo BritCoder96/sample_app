b5e608ff799b85029de9a271644ac95e2691e7c5
Bryan Benson <bryanbenson83@gmail.com>
2014-05-22 14:02:30 -0400

    Revised production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ SampleApp::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = flase
 
   # Generate digests for assets URLs.
   config.assets.digest = true
